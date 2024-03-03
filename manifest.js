0dccd164518d9ce62ece6abb397fc1538718f472
横茶横葉 <108619120+YokochaYokoha@users.noreply.github.com>
2024-03-03 16:27:06 +0900

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "CoffeeSub-PWA版Coffee",
+  "short_name": "CoffeeSub",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": [{
+    "src": "./lowres.webp",
+    "sizes": "48x48",
+    "type": "image/webp"
+  },{
+    "src": "./hd_hi.ico",
+    "sizes": "72x72 96x96 128x128 256x256"
+  }]
+}
---
+++ service-worker.js
@@ -0,0 +1,33 @@
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  './backpic3.png',
+  './csub.png',
+  '.hd_hi.ico'
+  './lowres.webp'
+];
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
